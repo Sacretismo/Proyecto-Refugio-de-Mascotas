3520155f820e8b6e18698759033cb124b3acf8e5
Danieltq04 <daniel.tq04@gmail.com>
2022-06-13 21:42:12 -0300

    Agregado de funcionalidades

---
+++ js/colabora.js
@@ -0,0 +1,14 @@
+
+
+document.querySelector("#btn-zaguates").addEventListener("click",()=>{
+    window.open("https://es-la.facebook.com/zaguates.refugio/")
+})
+document.querySelector("#btn-campito").addEventListener("click",()=>{
+    window.open("https://www.elcampitorefugio.org")
+})
+document.querySelector("#btn-patitas-al-rescate").addEventListener("click",()=>{
+    window.open("https://www.facebook.com/pg/Patitas-al-rescate-106957504300880/posts/")
+})
+document.querySelector("#btn-patitas-en-casa").addEventListener("click",()=>{
+    window.open("https://www.facebook.com/pages/category/Pet-Adoption-Service/Patitas-en-casa-2078991785646879/")
+})
